e0bd5912683d7e31c5bbd2428f896fcfc690284f
Mercurblende <elven.clarence+github@gmail.com>
2024-12-30 20:08:05 +0330

    days without infosec ruining something for everyone: 0

--- index.js
+++ index.js
@@ -1,102 +1,108 @@
-const express = require('express')
-const app = express()
-let morgan = require('morgan')
-const cors = require('cors')
-const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
+const express = require("express");
+const app = express();
+let morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 
-app.use(expressCspHeader({ 
-    policies: { 
-        'default-src': [expressCspHeader.NONE], 
-        'img-src': [expressCspHeader.SELF], 
-    } 
-}));  
+morgan.token("body", function (req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
 
-morgan.token('body', function (req, res) {if (req.method==='POST') return JSON.stringify(req.body)})
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
-app.use(morgan(' :method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'"],
+      },
+    },
+  }),
+);
+app.use(express.json());
+app.use(express.static("dist"));
+app.use(
+  morgan(
+    " :method :url :status :res[content-length] - :response-time ms :body",
+  ),
+);
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-// Every night I wonder why fly io had to be like this.
-// app.get('/', (req, res) => {
-//   res.redirect("/api/persons")
-// })
+// app.get("/", (req, res) => {
+//   res.sendFile("/dist/index.html");
+// });
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const note = persons.find(note => note.id === id)
-  if (note) { response.json(note) } else { response.status(404).end() }
-})
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const note = persons.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
 
-app.get('/info', (req, res) => {
-  population = persons.length 
-  time = Date()  
-  res.send(
-    `<p>Phonebook has info for ${population} people</p>  ${time}`
-  )
-})
+app.get("/info", (req, res) => {
+  population = persons.length;
+  time = Date();
+  res.send(`<p>Phonebook has info for ${population} people</p>  ${time}`);
+});
 
-app.post('/api/persons', (request, response) => {
-
+app.post("/api/persons", (request, response) => {
   if (!request.body.number || !request.body.name) {
     return response.status(400).json({
-      error: "Number or Name is missing."
-    })
-  }
-  else if ( persons.some(note => request.body.name === note.name) ) {
+      error: "Number or Name is missing.",
+    });
+  } else if (persons.some((note) => request.body.name === note.name)) {
     return response.status(400).json({
-      error: "Name already exists in Phonebook."
-    })
+      error: "Name already exists in Phonebook.",
+    });
   }
 
+  const note = request.body;
+  note.id = String(Math.random() * 10 ** 17);
 
-  const note = request.body
-  note.id = String(Math.random()*10**17)
-  
-  persons = persons.concat(note)
+  persons = persons.concat(note);
 
-  response.json(note)
-})
+  response.json(note);
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(note => note.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((note) => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-const PORT = process.env.PORT || 3001
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
